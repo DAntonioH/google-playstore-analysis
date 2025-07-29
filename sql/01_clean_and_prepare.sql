562a09a84300d2116d4830ae4b1e99079305c14e
Donald A Hernandez <donald10110@gmail.com>
2025-07-29 17:39:30 -0600

    Add SQL query to count unique app categories to data cleaning script

--- sql/01_clean_and_prepare.sql
+++ sql/01_clean_and_prepare.sql
@@ -8,3 +8,13 @@ WHERE App IS NOT NULL
   AND Type IS NOT NULL
   AND Price IS NOT NULL
   AND ContentRating IS NOT NULL;
+
+SELECT DISTINCT 
+    LOWER(REPLACE(Category, ' ', '_')) AS cleaned_category,
+    LOWER(REPLACE(Genres, ' ', '_')) AS cleaned_genre
+FROM 
+  googleplaystore_raw;
+
+SELECT COUNT(DISTINCT Category) AS unique_categories
+FROM  
+  googleplaystore_raw;
